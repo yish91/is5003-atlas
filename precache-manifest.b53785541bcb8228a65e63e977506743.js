855c3ee1bc75d4205389c1c405bc39fc54a74cd1
ys <ys@heli-cap.com>
2019-10-31 19:02:47 +0800

    Updates

---
+++ precache-manifest.b53785541bcb8228a65e63e977506743.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4368b74d5a160805031807ad6020d46d",
+    "url": "/is5003-atlas/index.html"
+  },
+  {
+    "revision": "a817f47cd72e89db00fd",
+    "url": "/is5003-atlas/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4f63bc6096ae888251b9",
+    "url": "/is5003-atlas/static/js/2.8b6a4258.chunk.js"
+  },
+  {
+    "revision": "a817f47cd72e89db00fd",
+    "url": "/is5003-atlas/static/js/main.4063c0c5.chunk.js"
+  },
+  {
+    "revision": "3a5e97e8194d85a482b0",
+    "url": "/is5003-atlas/static/js/runtime-main.1d9ab4dd.js"
+  },
+  {
+    "revision": "4af51259f1c941404fbef6fc7dce35a7",
+    "url": "/is5003-atlas/static/media/platform_revolution.4af51259.jpg"
+  }
+]);
